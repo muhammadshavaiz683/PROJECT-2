ff60b1772497003447b10123e1cedb9d4f521382
muhammadshavaiz683 <muhammad.shavaiz2006@gmail.com>
2024-10-21 13:37:06 +0500

    Add files via upload

---
+++ TASKS.cpp
@@ -0,0 +1,116 @@
+/*M.SHAVAIZ JAMIL*/
+//Lab 2
+
+//Task 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "***********" << endl;
+	cout << "HELLO WORLD" << endl;
+	cout << "***********" << endl;
+
+/*TASK 2*/
+	
+    cout << "***********" << endl;
+	cout << "*         *" << endl;
+	cout << "***********" << endl;
+
+/*TASK 3*/
+	
+	cout << "   *" << endl;
+	cout << "  * *" << endl;
+	cout << " *   *" << endl;
+	cout << "*******" << endl;
+	
+/*TASK 4*/
+	cout << "   *" << endl;
+	cout << "  ***" << endl;
+	cout << " *****" << endl;
+	cout << "*******" << endl;
+	
+
+	/*TASK 5*/
+	cout << " ****" << endl;
+	cout << "*    *" << endl;
+	cout << "*    *" << endl;
+	cout << " ****" << endl;
+	
+
+	/*TASK 6*/
+
+	cout << "********" << endl;
+	cout << "*      *" << endl;
+	cout << "*      *" << endl;
+	cout << "********" << endl;
+
+
+  /*TASK 7*/
+	cout << "   *" << endl;
+	cout << " *   *" << endl;
+	cout << "*     *" << endl;
+	cout << " *   *" << endl;
+	cout << "   *" << endl;
+
+	/*TASK 8*/
+	cout << "    *" << endl;
+	cout << " *******" << endl;
+	cout << "**********" << endl;
+	cout << " *******" << endl;
+	cout << "    *" << endl;
+
+	/*TASK 9*/
+	cout << "@@@@@@@@@@@@" << endl;
+	cout << "@  @\t@  @" << endl;
+	cout << "@ @\t @ @" << endl;
+	cout << "@@\t  @@" << endl;
+	cout << "@\t   @" << endl;
+	cout << "@@@@@@@@@@@@" << endl;
+
+	/*TASK 10*/
+	cout << "..........." << endl;
+	cout << "     ." << endl;
+	cout << "     ." << endl;
+	cout << "     ." << endl; 
+	cout << "     ." << endl; 
+	cout << "     ." << endl;
+	cout << "............" << endl;
+	cout << "............" << endl;
+	cout << "     ." << endl;
+	cout << "     ." << endl;
+	cout << "     ." << endl;
+	cout << "     ." << endl;
+	cout << "     ." << endl;
+	cout << "............" << endl;
+	cout << "." << endl;
+	cout << "." << endl;
+	cout << "." << endl;
+	cout << "." << endl;
+	cout << "." << endl;
+	cout << "." << endl; 
+	cout << "............." << endl;
+	
+	/*TASK 11*/
+	cout << "Name:M.SHAVAIZ JAMIL" << endl;
+	cout << "University ID:L1F24BSCS0147" << endl;
+	cout << "Father Name:MUHAMMAD JAMIL" << endl;
+	cout << "Phone Number:03298091977" << endl;
+	cout << "Address:H#1197/C,DEHLI ROAD,SADDAR, LAHORE CANTT." << endl;
+	cout << "Semester:1" << endl;
+	cout << "Course:Introduction To Computing Lab" << endl;
+
+	/*TASK 12*/
+	
+	cout << "*****************************************" << endl;
+	cout << "*\tProgramming Assignment 1\t*" << endl;
+	cout << "*\tComputer Programming I\t\t*" << endl;
+	cout << "*\t      Author:???\t\t*" << endl;
+	cout << "*\tDue Date: Thursday,Jan. 24\t*" << endl;
+	cout << "*****************************************" << endl;
+	
+	
+	
+
+	return 0;
+}
